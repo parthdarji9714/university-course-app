7ea09a91c1ad64c5bad356dcfa146cb2dbf9a055
Parth Darji <parthdarji9714@gmail.com>
2024-08-18 02:19:23 -0400

    Update main function for heroku deployment

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from flask_cors import CORS
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
 
@@ -215,10 +216,9 @@ def create_course():
     return jsonify({"success": "Course created successfully", "course_id": course_id}), 201
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
 if __name__ == '__main__':
     check_and_refresh_data()  # Check data expiration on app startup
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host = '0.0.0.0', port = port)
