81559a74f90f2758064456bb7ac4287ddb9c5cc1
Parth Darji <parthdarji9714@gmail.com>
2024-08-18 15:25:36 -0400

    Update url in frontend

--- frontend/src/app/course.service.ts
+++ frontend/src/app/course.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class CourseService {
   // private baseUrl = 'http://localhost:5000/api/courses'; // Replace with your backend URL
   //private baseUrl = 'http://localhost:5000/'; // Replace with your backend URL
 
-  private baseUrl = 'https://pure-depths-08517-1676a3ff0ea5.herokuapp.com/'; // Replace with your backend URL
+  private baseUrl: string = environment.apiUrl; // Replace with your backend URL
   constructor(private http: HttpClient) { }
 
   // Fetch courses with pagination
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://your-heroku-app-name.herokuapp.com/'
 };
